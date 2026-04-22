264d8e93e12170f833af87fed29e8b2830c4e421
DANIEL LUIS BORTOLIN DA SILVA <02277887021@senacrs.edu.br>
2026-04-22 09:17:36 -0300

    aula, framework

---
+++ react/react/src/App.jsx
@@ -0,0 +1,15 @@
+import {Header} from "./components/Header"
+import {Main} from "./components/Main"
+import {Footer} from "./components/Footer"
+import "./index.css"
+
+function App () {
+return (
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
+)
+
+}
---
+++ react/react/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer () {
+    return (
+        <rodape>
+            <p>Primeiro projeto React</p>
+        </rodape>
+    )
+}
+
+
+export default Footer
